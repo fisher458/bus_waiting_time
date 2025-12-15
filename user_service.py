f09d8b03ee9314c0b8749c6047ffbe64f2c17b5b
Adam fisher <adamfisher@AdamdeMac-Studio.local>
2025-12-15 16:29:38 +0800

    測user

--- user_service.py
+++ user_service.py
@@ -3,7 +3,7 @@ import probability
 if __name__ == "__main__":
     station_name = input("哪一站")
     estimate_time = int(input("顯示等多久"))
-    direction = int(input("往動物園輸入1,台北車站輸0"))
+    direction = int(input("往動物園輸入1,台北車站輸0\n"))
     
     time_error, lenth = probability.detect(station_name,direction,estimate_time)
     mean, std = probability.cal(time_error)
